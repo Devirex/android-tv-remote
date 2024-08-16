865ee47a45d339388323e7463c513702093454a9
Devirex <naibaf_11@yahoo.de>
2024-08-16 14:43:28 +0200

    module android_tv_remote

--- android_tv_remote.py
+++ android_tv_remote.py
@@ -8,56 +8,40 @@ from androidtvremote2 import (
     InvalidAuth,
 )
 
-
 class android_tv_remote(generic.FhemModule):
     def __init__(self, logger):
         super().__init__(logger)
 
     # FHEM FUNCTION
     async def Define(self, hash, args, argsh):
+        self.logger.error(f"{len(args)}")
+        if len(args) < 4:
+            await fhem.readingsSingleUpdate(hash, "state", "Usage: define remote fhempy android_tv_remote (ip or dns name) -> Please delete and redefine", 1)
+            return "Usage: define remote fhempy android_tv_remote (ip or dns name)"
         await super().Define(hash, args, argsh)
-        self.logger.error(f"Defined android TV Reomte")
-
+        self.logger.error(f"Defined android TV Remote")
+        
+        
+        
         attr_config = {
-            "interval": {
-                "default": 100,
-                "format": "int",
-                "help": "Change interval, default is 100.",
+            "tv": {
+                "format": "string",
+                "help": "set Android TV IP or dns Name",
             }
         }
         await self.set_attr_config(attr_config)
 
         set_config = {
-            "mode": {
-                "args": ["mode"],
-                "argsh": ["mode"],
-                "params": {"mode": {"default": "eco", "optional": False}},
-                "options": "eco,comfort",
-            },
-            "desiredTemp": {"args": ["temperature"], "options": "slider,10,1,30"},
-            "holidayMode": {
-                "args": ["endday", "endtime", "temperature"],
-                "params": {
-                    "endday": {"default": "31.12.2030"},
-                    "endtime": {"default": "23:59"},
-                    "temperature": {"default": 21, "format": "int"},
-                },
-            },
-            "on": {
-                "args": ["seconds"],
-                "params": {
-                    "seconds": {"default": 0, "optional": True, "format": "int"}
-                },
-                "help": "Specify seconds as parameter to change to off after X seconds.",
-            },
-            "off": {},
+            "register": {
+                "args": ["code"],
+                "params": { "code": { "default": "" , "optional": True }},
+            }
         }
         await self.set_set_config(set_config)
-
-        if len(args) > 3:
-            return "Usage: define hello_fhempy fhempy helloworld"
+        
+        self.hostname =  args[3]
         await fhem.readingsBeginUpdate(hash)
-        await fhem.readingsBulkUpdateIfChanged(hash, "state", "on")
+        await fhem.readingsBulkUpdateIfChanged(hash, "state", f"connecting to {self.hostname}...")
         await fhem.readingsEndUpdate(hash, 1)
 
     # Attribute function format: set_attr_NAMEOFATTRIBUTE(self, hash)
@@ -68,26 +52,7 @@ class android_tv_remote(generic.FhemModule):
         pass
 
     # Set functions in format: set_NAMEOFSETFUNCTION(self, hash, params)
-    async def set_on(self, hash, params):
-        # params contains the keyword which was defined in set_list_conf for "on"
-        # if not provided by the user it will be "" as defined in set_list_conf (default = "" and optional = True)
-        seconds = params["seconds"]
-        if seconds != 0:
-            await fhem.readingsSingleUpdate(hash, "state", "on " + str(seconds), 1)
-        else:
-            await fhem.readingsSingleUpdate(hash, "state", "on", 1)
-
-    async def set_off(self, hash, params):
-        # no params argument here, as set_off doesn't have arguments defined in set_list_conf
-        await fhem.readingsSingleUpdate(hash, "state", "off", 1)
-        self.create_async_task(self.long_running_task())
-        return ""
-
-    async def long_running_task(self):
-        await asyncio.sleep(30)
-        await fhem.readingsSingleUpdate(self.hash, "state", "long running off", 1)
-
-    async def set_mode(self, hash, params):
+    async def set_register(self, hash, params):
         # user can specify mode as mode=eco or just eco as argument
         # params['mode'] contains the mode provided by user
         mode = params["mode"]
